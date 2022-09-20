8613d992a54a21b40cdd1bebf69349a1a75daf20
Mateo Chavez <113551958+mat212121@users.noreply.github.com>
2022-09-20 19:08:49 +0000

    a,b,c,d

--- main.py
+++ main.py
@@ -23,20 +23,24 @@ my_tuple = (1, 2, (10, 20), 4)
 print(my_tuple[2][1])
 # place these numbers in separate variables from the tuple below
 t = (1, 2, 3)
-
+x,y,z = t
+print(x,y,z)
 #get me the length of the above tuple --- there are two ways of doing this... len(), count()...  use count if you want to get how many times an item appears in a tuple
-
+print(len(my_tuple))
 # Use a tuple method to count the number of times the value 2 appears in the following tuple, and display the result (integer) on the screen:
 
 my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+print(my_tuple.count(2))
 
 # Convert the following tuple to a list, and store it in a variable called my_list.
 
 my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
-
+my_list = list(my_tuple)
+print(my_list)
 #Extract the elements of the following tuple into four variables: a, b, c, d
 
 my_tuple = (1, 2, 3, 4)
+my_tuple = a,b,c,d
 
 
 #################################################sets######################################################

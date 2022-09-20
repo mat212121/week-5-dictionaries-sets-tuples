946bb28d1e3221de7351a87e40fd9732be766f10
Mateo Chavez <113551958+mat212121@users.noreply.github.com>
2022-09-20 19:17:20 +0000

    remove/add

--- main.py
+++ main.py
@@ -39,30 +39,38 @@ my_list = list(my_tuple)
 print(my_list)
 #Extract the elements of the following tuple into four variables: a, b, c, d
 
-my_tuple = (1, 2, 3, 4)
-my_tuple = a,b,c,d
+# my_tuple = (1, 2, 3, 4)
+# my_tuple = a,b,c,d
 
 
 #################################################sets######################################################
-
-
+#sets are similiar to tuples.the only difference is that they will 
+#not contain or print out any repeated elements
+my_firstSet = set((1,2,3,4,5))
+print(my_firstSet)
+print(6 in my_firstSet)
 # Join the following sets into one, called my_set_3:
 
-{1, 2, "three", "four"}
+set1 = {1, 2, "three", "four"}
 
-{"three", 4, 5}
+set2 = {"three", 4, 5}
 
-
+s3 = set1.union(set2)
+print(s3)
+s3.add(8)
+print(s3)
 # Remove a random item from the following set, using set methods.
 
 raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
-
+raffle.remove("Phoebe")
+print(raffle)
 
 
 # Add the name Gunther to the following set, using set methods:
 
 raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
-
+raffle.add(Gunther)
+print(raffle)
 #################################################booleans######################################################
 # booleans
 # a boolean can only have 2 values -- true or False
